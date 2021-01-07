8e2866d29473d0bac936c3dab2a2a161162484c7
Malcolm Idlebird <malcolmidlebird@Malcolms-MacBook-Pro.local>
2021-01-06 22:50:49 -0700

    PasswordGenerater fuction build

--- script.js
+++ script.js
@@ -15,4 +15,39 @@ generateBtn.addEventListener("click", writePassword);
 
 
 //Generator Function 
-funtion 
+funtion generatePassword() {
+
+  //window alert informing the users about the password criteria
+  window.alert("Select the password character type(s) you should include.\n (lower & upper case letters, numbers, and special characters");
+
+  //dialog boxes that comfirms the users criteria selection
+
+  var upCase = confirm("Do you want to include upper case letters? \n A-Z");
+  var lowCase = confirm("Do you want to include lower case letters? \n a-z");
+  var num = confirm("Do you want to include numbers? \n 0-9");
+  var spec = confirm("Do you want to include special characters? \n !@#$%^&*()-_=+,./<>?[]{}`~");
+
+  //store the user selection
+
+  var userChoices = upCase + lowCase + num + spec;
+  console.log("Number of user choices selected: "+ userChoices);
+
+  if (userChoices === 0){
+    return alert("You have to pick at least ONE charactor type.");
+
+  }
+  console.log("Upper Case Letters: " + upCase);
+  console.log("Lower Case Letters: " + lowCase);
+  console.log("Numbers: " + num);
+  console.log("Special Charactors: " + spec);
+
+  //ask users for the desired password string length
+
+  var passLenght = prompt("How many charactors do you want your password to be? \n Password must have a lenght between 8-128");
+
+  if(passLenght === null){
+    return;
+  }
+  
+
+}
